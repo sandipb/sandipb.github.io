e79d5fe86252132816f6b77bb1ada2edae244183
Sandip Bhattacharya <sandipb@sandipb.net>
2017-12-07 19:33:38 -0800

    Switch website

---
+++ fabfile.py
@@ -0,0 +1,15 @@
+from fabric.api import local, warn_only
+from datetime import datetime
+
+
+def deploy():
+    "Generate web pages and push to web site"
+    with warn_only():
+      local("hugo")
+    local("rsync -avzr --delete --progress public/ sandipb@sandipb.net:ref.sandipb.net/")
+
+
+def newpost(slug):
+    "Create a new post(fab newpost:SLUG)"
+    today = datetime.now().strftime("%Y-%m-%d")
+    local("hugo new post/%s-%s.markdown" % (today, slug))
